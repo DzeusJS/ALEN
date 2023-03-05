fe2686a7c28c574798e43c744a9c612d3b39f2c6
ooo <ooo@DESKTOP-B23U12A>
2023-03-05 12:00:55 +0200

    Add project files.

---
+++ ALEN/UserDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ALEN
+{
+    public class UserDto //user data transfer object
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+
+
+
+    }
+}
